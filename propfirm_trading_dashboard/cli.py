8d8e1c07d75137312a53092618502d6510b51441
João Pereira <joaopereira9913@gmail.com>
2026-02-05 13:58:42 +0000

    Created metrics calculator class and execution on cli file

--- propfirm_trading_dashboard/cli.py
+++ propfirm_trading_dashboard/cli.py
@@ -1,7 +1,15 @@
 from csv_parser import load_csv_file, validate_columns
+from metrics import MetricsCalculator as mc
 
-filename = "test_fail.csv"
+phase_type = "phase1"
+filename = phase_type + ".csv"
 path = "data/" + filename
 
+#Load and validate columns of csv file
 df = load_csv_file(path)
-print(validate_columns(df, filename))
+validate_columns(df, filename)
+
+#Calculate metrics of csv file
+calculator = mc(df)
+
+calculator.calculate_metrics(phase_type)
---
+++ propfirm_trading_dashboard/metrics.py
@@ -0,0 +1,34 @@
+class MetricsCalculator:
+    def __init__(self, df):
+        self.df = df
+    
+    def calculate_metrics(self, phase_type: str):
+        dispatch_types = {
+        "phase1": self._calculate_metrics_phase1,
+        "phase2": self._calculate_metrics_phase2,
+        "phase3": self._calculate_metrics_phase3,
+        "challenge": self._calculate_metrics_challenge,
+        "funded": self._calculate_metrics_funded,
+        }
+
+        if phase_type not in dispatch_types:
+            raise ValueError("Invalid phase_type")
+        
+        return dispatch_types[phase_type]()
+
+
+    #Private methods for calculating metrics depending on phase
+    def _calculate_metrics_phase1(self):
+        print("Calculating Phase 1")
+
+    def _calculate_metrics_phase2(self):
+        print("Calculating Phase 2")
+
+    def _calculate_metrics_phase3(self):
+        print("Calculating Phase 3")
+
+    def _calculate_metrics_challenge(self):
+        print("Calculating Challenge")
+
+    def _calculate_metrics_funded(self):
+        print("Calculating Funded")
